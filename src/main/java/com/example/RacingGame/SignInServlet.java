08461d0eca57d045a052181164c0de3c69256a26
lbk2kgit <71619375+bin98123@users.noreply.github.com>
2021-01-15 17:33:25 +0700

    1

--- src/main/java/com/example/RacingGame/SignInServlet.java
+++ src/main/java/com/example/RacingGame/SignInServlet.java
@@ -17,7 +17,7 @@ import javax.servlet.http.HttpSession;
  * Servlet implementation class UserProfile
  */
 @WebServlet("/LoginServlet")
-public class LoginServlet extends HttpServlet {
+public  class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
