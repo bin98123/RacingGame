5cf38c84ae71eaa30667bf99c05fdf8f42263509
lbk2kgit <71619375+bin98123@users.noreply.github.com>
2021-01-16 15:06:53 +0700

    Update SignUpServlet.java

--- src/main/java/com/example/RacingGame/SignUpServlet.java
+++ src/main/java/com/example/RacingGame/SignUpServlet.java
@@ -74,7 +74,7 @@ public class SignUpServlet extends HttpServlet {
 
 		try {
 			UserDao userDao = new UserDao();
-			if (ok = true && userDao.registerUser(userDetails) != 0) {
+			if (ok == true && userDao.registerUser(userDetails) != 0) {
 
 				response.sendRedirect("/RacingGame/WelcomeUser.jsp");
 			} else {
